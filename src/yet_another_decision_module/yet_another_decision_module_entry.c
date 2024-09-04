5ea91be91fdd6ec202416b523895680c3d32574d
Никита <103407183+Nikitaqe@users.noreply.github.com>
2024-09-04 17:26:41 +0700

    Add files via upload

---
+++ src/yet_another_decision_module/yet_another_decision_module_entry.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../data_libs/data_io.h"
+#include "../data_libs/data_stat.h"
+#include "decision.h"
+
+void main() {
+    double *data;
+    int n;
+    input(&data, &n);
+    if (make_decision(data, n))
+        printf("YES");
+    else
+        printf("NO");
+    free(data);
+}
